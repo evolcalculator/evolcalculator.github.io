85af2a95322aaaaa5db5b9e406144dec4669036b
evolcalculator <zhengshuyu1996@163.com>
2018-02-14 14:01:25 +0800

    add cards

--- js/calculator.js
+++ js/calculator.js
@@ -101,9 +101,13 @@ function requestData(){
         url: "http://39.107.72.254/lyzz/card/sort/",
         data: {type: tmp_category, name: encodeURI(tmp_name), person: encodeURI(tmp_character), star: star_num},
         dataType: "jsonp",
+        error: function () {
+            alert("抱歉！服务器出错了QAQ");
+            return;
+        },
         success: function(data){
-            if ( data == null || data.length == 0 || data == "" || data.length == 1) {
-                document.getElementById('result').innerHTML = "<div style='padding-left: 20px'>未找到该关卡：" + name + " " + "/" + person + "</div>";
+            if (data == null || data.length == 0 || data == "" || data.length == 1) {
+                alert("抱歉！服务器出错了QAQ");
                 return;
             }
             w_decision = data["decision"];
